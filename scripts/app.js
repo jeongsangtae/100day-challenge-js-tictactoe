b1f86e8846d825de6d064bf3d5df84de9b0b3f2c
jeongsangtae <wjdtkdxo55@naver.com>
2023-01-27 05:12:54 +0900

    Tic Tac Toe HTML-script, JS-eventListener

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+// 여러가지를 초기화시키는 가장 먼저 실행되는 파일
+
+const editPlayer1Btn = document.getElementById("edit-player-1-btn");
+const editPlayer2Btn = document.getElementById("edit-player-2-btn");
+
+editPlayer1Btn.addEventListener("click", openPlayerConfig);
+editPlayer2Btn.addEventListener("click", openPlayerConfig);
