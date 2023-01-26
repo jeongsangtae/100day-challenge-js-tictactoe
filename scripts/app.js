67945f49a63120755a1c7e20e2d2b56552f8431a
jeongsangtae <wjdtkdxo55@naver.com>
2023-01-27 05:54:03 +0900

    End of Day 41(open event, close event)

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,15 @@
 // 여러가지를 초기화시키는 가장 먼저 실행되는 파일
 
+const playerConfigOverlay = document.getElementById("config-overlay");
+const backdrop = document.getElementById("backdrop");
+
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
 
+const cancelBtn = document.getElementById("cancel-config-btn");
+
 editPlayer1Btn.addEventListener("click", openPlayerConfig);
 editPlayer2Btn.addEventListener("click", openPlayerConfig);
+
+cancelBtn.addEventListener("click", closePlayerConfig);
+backdrop.addEventListener("click", closePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,12 @@
 // 모든 로직을 담당하는 파일
 // 사용자 입력을 받아서 유효성을 검사하고 저장하는 파일
 
-function openPlayerConfig() {}
+function openPlayerConfig() {
+  playerConfigOverlay.style.display = "block";
+  backdrop.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlay.style.display = "none";
+  backdrop.style.display = "none";
+}
