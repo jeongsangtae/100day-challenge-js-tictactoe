7c2e044099af98bf490ddad4011ac28bff3bd71c
jeongsangtae <wjdtkdxo55@naver.com>
2023-01-27 23:01:00 +0900

    JS-game start logic add

--- scripts/app.js
+++ scripts/app.js
@@ -16,11 +16,14 @@ const playerConfigOverlay = document.getElementById("config-overlay");
 const backdrop = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorOutput = document.getElementById("config-errors");
+const gameArea = document.getElementById("active-game");
 
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
 
 const cancelBtn = document.getElementById("cancel-config-btn");
+
+const startNewGameBtn = document.getElementById("start-game-btn");
 
 editPlayer1Btn.addEventListener("click", openPlayerConfig);
 editPlayer2Btn.addEventListener("click", openPlayerConfig);
@@ -29,3 +32,5 @@ cancelBtn.addEventListener("click", closePlayerConfig);
 backdrop.addEventListener("click", closePlayerConfig);
 
 formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameBtn.addEventListener("click", startNewGame);
--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,9 @@
 // 게임 로직을 담당하는 파일
+
+function startNewGame() {
+  if (players[0].name === "" || players[1].name === "") {
+    alert("플레이어 이름을 입력해주세요.");
+    return;
+  }
+  gameArea.style.display = "block";
+}
