3a886e2d794642863c92f36dec2f4bb711a7fdfc
jeongsangtae <wjdtkdxo55@naver.com>
2023-01-27 23:53:54 +0900

    JS-game round & field select

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 // 여러가지를 초기화시키는 가장 먼저 실행되는 파일
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
@@ -17,13 +18,14 @@ const backdrop = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorOutput = document.getElementById("config-errors");
 const gameArea = document.getElementById("active-game");
+const activePlayerName = document.getElementById("active-player-name");
 
 const editPlayer1Btn = document.getElementById("edit-player-1-btn");
 const editPlayer2Btn = document.getElementById("edit-player-2-btn");
-
 const cancelBtn = document.getElementById("cancel-config-btn");
-
 const startNewGameBtn = document.getElementById("start-game-btn");
+// const gameFields = document.querySelectorAll("#game-board li");
+const gameBoard = document.getElementById("game-board");
 
 editPlayer1Btn.addEventListener("click", openPlayerConfig);
 editPlayer2Btn.addEventListener("click", openPlayerConfig);
@@ -34,3 +36,9 @@ backdrop.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGameBtn.addEventListener("click", startNewGame);
+
+// for (const gameField of gameFields) {
+//   gameField.addEventListener("click", selectGameField);
+// }
+
+gameBoard.addEventListener("click", selectGameField);
--- scripts/game.js
+++ scripts/game.js
@@ -5,5 +5,25 @@ function startNewGame() {
     alert("플레이어 이름을 입력해주세요.");
     return;
   }
+  activePlayerName.innerHTML = players[activePlayer].name;
   gameArea.style.display = "block";
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerName.innerHTML = players[activePlayer].name;
+}
+
+function selectGameField(selectEvent) {
+  if (selectEvent.target.tagName !== "LI") {
+    return;
+  }
+
+  selectEvent.target.innerHTML = players[activePlayer].symbol;
+  selectEvent.target.classList.add("disabled");
+  switchPlayer();
 }
